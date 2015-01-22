e46815416455f3136a7b9bdb794be7c7f6c0af9f
bound1ess <bound1ess.dev@gmail.com>
2015-01-22 18:16:11 +0600

    Add rectangle.sh

---
+++ src/rectangle.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+source ./src/helpers.sh
+
+SIDE1=30
+SIDE2=$(($SIDE1/2))
+
+# Print a new line.
+printf "\n"
+
+# Side #1
+repeat-string "[]" $SIDE1
+indent-string $SIDE2
+
+INDEX=0
+
+while (($(($SIDE2-2)) > $INDEX)); do
+    # Render sides #2 and #3
+    repeat-string " " $(($((SIDE1-2))*2))
+    repeat-string "[]${BUFFER}[]" 1
+
+    indent-string $SIDE2
+
+    INDEX=$(($INDEX+1))
+done
+
+# Side #4
+repeat-string "[]" $SIDE1
+indent-string $SIDE2
+
+# A new line character again.
+printf "\n"
