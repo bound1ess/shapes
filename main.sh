1bd9ed5d9961999d8508e147bf40b57dcef8bc36
bound1ess <bound1ess.dev@gmail.com>
2015-01-22 21:26:18 +0600

    Update logic.

--- main.sh
+++ main.sh
@@ -3,8 +3,17 @@
 # Include Selectable.
 source ./selectable.sh
 
-# Clear the screen.
-printf "\ec"
+include-source-file() {
+    # Include the appropriate source file.
+    source "./src/$(tr " " "-" <<< ${SHAPE,,}).sh"
+}
 
-# Include the appropriate source file.
-source "./src/$(tr " " "-" <<< ${SHAPE,,}).sh"
+include-source-file
+sleep 10
+
+while true; do
+    selectable-run
+    include-source-file
+
+    sleep 10 # Give the user 10 seconds.
+done
